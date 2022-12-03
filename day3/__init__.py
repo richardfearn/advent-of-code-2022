ab53e3d587375c1223ec7b04ffac891878228b94
Richard Fearn <richardfearn@gmail.com>
2022-12-03 10:18:30 +0000

    Day 3 - set improvements

--- day3/__init__.py
+++ day3/__init__.py
@@ -1,11 +1,9 @@
-from functools import reduce
-
-
 def part_1_answer(lines):
     def common_item(rucksack):
         mid = len(rucksack) // 2
         first, second = set(rucksack[:mid]), set(rucksack[mid:])
-        return next(iter(first.intersection(second)))
+        common_items = first & second
+        return common_items.pop()
 
     return sum(priority(common_item(rucksack)) for rucksack in lines)
 
@@ -13,8 +11,8 @@ def part_1_answer(lines):
 def part_2_answer(lines):
     def common_item(rucksacks):
         rucksacks = [set(r) for r in rucksacks]
-        common_items = reduce(set.intersection, rucksacks)
-        return next(iter(common_items))
+        common_items = set.intersection(*rucksacks)
+        return common_items.pop()
 
     return sum(priority(common_item(lines[i:i + 3])) for i in range(0, len(lines), 3))
 
